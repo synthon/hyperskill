19a7eec09b4d0b7ba42cf8dbc7776d9d9fd264d4
SYNTH <40410117+synthon@users.noreply.github.com>
2020-02-12 00:11:26 +0300

    +

---
+++ Java/Tasks/Basic syntax and simple programs/Comments/UncommentLine.java
@@ -0,0 +1,9 @@
+/**
+ * The class implements a simple application with the main method.
+ */
+class CommentedSimpleApplication {
+
+    public static void main(String[] args) {
+            System.out.println(893457);
+    }
+}
---
+++ Java/Tasks/Basic syntax and simple programs/Types and variables/Assignments.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String a = "3";
+        String b = "5";
+        String c = "4";
+
+        System.out.println(a + " " + b + " " + c);
+    }
+}
