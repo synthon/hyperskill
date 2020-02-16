3bdae9aa73f6f25ca15e66544bdad5deda655e2e
SYNTH <40410117+synthon@users.noreply.github.com>
2020-02-16 16:41:24 +0300

    flaw

---
+++ Java/Tasks/Basic syntax and simple programs/Strings/DoubleCharacters.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            System.out.print(input.charAt(i));
+            System.out.print(input.charAt(i));
+        }
+    }
+}
