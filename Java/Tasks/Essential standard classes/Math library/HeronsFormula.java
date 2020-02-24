5fd1d837677371477ec2144dbffc49cdf87d0a4f
SYNTH <40410117+synthon@users.noreply.github.com>
2020-02-24 12:37:27 +0300

    add task: heron's formula

---
+++ Java/Tasks/Essential standard classes/Math library/HeronsFormula.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        double p = (a + b + c) / 2;
+
+        double triangleArea = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+        System.out.println(triangleArea);
+    }
+}
