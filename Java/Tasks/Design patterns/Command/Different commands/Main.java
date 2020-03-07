a610ddafb57ecf981d983d62fdfcbdb0ba732acb
SYNTH <40410117+synthon@users.noreply.github.com>
2020-03-07 18:01:17 +0300

    add task: different commands

---
+++ Java/Tasks/Design patterns/Command/Different commands/Main.java
@@ -0,0 +1,43 @@
+interface Movable {
+    int getX();
+    int getY();
+    void setX(int newX);
+    void setY(int newY);
+}
+
+interface Storable {
+    int getInventoryLength();
+    String getInventoryItem(int index);
+    void setInventoryItem(int index, String item);
+}
+
+interface Command {
+    void execute();
+}
+
+class CommandMove implements Command {
+    Movable entity;
+    int xMovement;
+    int yMovement;
+
+    @Override
+    public void execute() {
+        entity.setX(entity.getX() + xMovement);
+        entity.setY(entity.getY() + yMovement);
+    }
+}
+
+class CommandPickItem implements Command {
+    Storable entity;
+    String item;
+
+    @Override
+    public void execute() {
+        for (int i = 0; i < entity.getInventoryLength(); i++) {
+            if (entity.getInventoryItem(i) == null) {
+                entity.setInventoryItem(i, item);
+                break;
+            }
+        }
+    }
+}
