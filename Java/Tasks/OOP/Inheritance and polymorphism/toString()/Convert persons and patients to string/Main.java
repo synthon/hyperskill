0e1083038712344c1bfa974633c595052887e247
SYNTH <40410117+synthon@users.noreply.github.com>
2020-03-09 03:12:38 +0300

    add tasks: toString()

---
+++ Java/Tasks/OOP/Inheritance and polymorphism/toString()/Convert persons and patients to string/Main.java
@@ -0,0 +1,30 @@
+class Person {
+
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person{name=%s,age=%d}", name, age);
+    }
+}
+
+class Patient extends Person {
+
+    protected int height;
+
+    public Patient(String name, int age, int height) {
+        super(name, age);
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Patient{name=%s,age=%d,height=%d}", name, age, height);
+    }
+}
---
+++ Java/Tasks/OOP/Inheritance and polymorphism/toString()/Convert vehicles and cars to string/Main.java
@@ -0,0 +1,28 @@
+class Vehicle {
+
+    protected String licensePlate;
+
+    public Vehicle(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Vehicle{licensePlate=%s}", licensePlate);
+    }
+}
+
+class Car extends Vehicle {
+
+    protected int numberOfSeats;
+
+    public Car(String licensePlate, int numberOfSeats) {
+        super(licensePlate);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Car{licensePlate=%s,numberOfSeats=%d}", licensePlate, numberOfSeats);
+    }
+}
